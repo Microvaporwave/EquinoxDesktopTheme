31494c75ee56dc90ff9b7d07f6aec4418a3fb754
Microvaporwave <sorry.wanna.remain@anonymous.com>
2022-03-14 20:13:40 +0100

    Added automatic font download  & install

--- autoinstaller.sh
+++ autoinstaller.sh
@@ -3,21 +3,32 @@
 # Clumsily Written By Microvaporwave
 
 # Copy icons to icon folder
-mkdir ~/.icons
+cp -r equinox-icons ~/.icons
 
 # Move GTK theme(s) to theme folder
 cp -r equinox-gtk ~/.themes
 
-# Copy Polybar config to config folder & optionally install polywins in required location
+# Copy Polybar config to config folder & optionally install polywins & JetBrainsMono in required locations
 cp -r polybar  ~/.config
+
+read -p "Automatically download & install JetBrainsMono font? [y/N] " yn;
+case $yn in
+        [yY] ) echo Downloading and installing font;
+               	wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/JetBrainsMono.zip -P ~/Downloads;
+		sudo mkdir /usr/share/fonts/JetBrainsMono && sudo unzip ~/Downloads/JetBrainsMono.zip -d /usr/share/fonts/JetBrainsMono;
+                exit;;
+        [nN] ) echo skipping...;
+                exit;;
+        * ) echo invalid response;
+esac
 
 read -p "Install polywins in required location? [y/N] " yn;
 
 case $yn in
 	[yY] ) echo cloning polywins;
 		git clone https://github.com/tam-carre/polywins.git ~/.config/polybar/scripts/polywins;
-		sed -i  's|active_text_color="#250F0B"|active_text_color="#ECB3B2"|' ~/.config/polybar/scripts/polywins/polywins.sh
-		sed -i  's|inactive_text_color="#250F0B"|inactive_text_color="#ECB3B2"|' ~/.config/polybar/scripts/polywins/polywins.sh
+		sed -i  's|active_text_color="#250F0B"|active_text_color="#ECB3B2"|' ~/.config/polybar/scripts/polywins/polywins.sh;
+		sed -i  's|inactive_text_color="#250F0B"|inactive_text_color="#ECB3B2"|' ~/.config/polybar/scripts/polywins/polywins.sh;
 		exit;;
 	[nN] ) echo skipping...;
 		exit;;
